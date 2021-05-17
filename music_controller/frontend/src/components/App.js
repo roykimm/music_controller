e7addd2f04fed11dce0508aafc3d116de8ede950
kimmyunghoon <roykimmyunghoon@gmail.com>
2021-05-18 00:10:45 +0900

    tests

---
+++ music_controller/frontend/src/components/App.js
@@ -0,0 +1,23 @@
+import React , { Component } from 'react';
+import { render } from 'react-dom';
+import HomePage from './HomePage';
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    } 
+
+    render (){
+        return (
+            <div className="center">
+                <HomePage />
+            </div>
+        )
+    }
+}
+
+export default App;
+
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
---
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { TextField, Button, Grid, Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+
+export class RoomJoinPage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state ={
+            roomCode : "",
+            error : ""
+        }
+    }
+
+    render() {
+        return (
+            <Grid container spacing={1} >
+                <Grid item xs={12} align="center">
+                    <Typography variant="h4" component="h4">
+                        Join a Room
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <TextField 
+                        error={this.state.error}
+                        label="Code"
+                        placeholder="Enter a Room Code"
+                        value={this.state.roomCode}
+                        helperText={this.state.error}
+                        variant="outlined"
+                    />
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button variant="contained" color="primary" onClick>
+                        Enter Room
+                    </Button>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button variant="contained" color="secondary" to="/" component={Link}>Back</Button>
+                </Grid>
+            </Grid>
+        )
+    }
+}
+
+export default RoomJoinPage
