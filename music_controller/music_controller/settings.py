dd3a0624a6377c4c7a4a56f9d701ba50f0c7da19
kimmyunghoon <roykimmyunghoon@gmail.com>
2021-05-23 21:57:27 +0900

    5

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'music_controller.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': Path(__file__).resolve().parent.parent / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- music_controller/spotify/crudentials.py
+++ music_controller/spotify/crudentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID = '4f4a0121182a40b8ad64c3cad1f9c621'
 CLIENT_SECRET = 'b668a796677540ffa2aece30185377a4'
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
+REDIRECT_URI = "http://gobella.ml:7000/spotify/redirect"
